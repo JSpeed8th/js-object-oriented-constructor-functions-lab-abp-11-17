33db743aa04f647d98234f599ea102d861528fc3
JSpeed8th <jspeed8th@gmail.com>
2018-01-21 22:40:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+/*Create a constructor function for each of the following:
++ Scooter with `year`, `color`, and `model` properties
++ Driver with `name`, `age`, and `experience` properties
++ PickupLocation with `address` and `city` properties*/
+
+function Scooter (year, color, model) {
+  
+}
